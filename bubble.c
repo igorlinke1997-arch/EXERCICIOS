87b617517ca062e7022b39f04f9db78d639ba27f
Igor Vargas <igorlinke1997@gmail.com>
2026-05-03 22:54:22 -0300

    Atualizando Bubble Sort com melhorias e formatação

--- bubble.c
+++ bubble.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
 
-int main() {
-    int vet[5] = {82, 45, 12, 78, 23};
-    int i, j, temp;
+// Função auxiliar para imprimir o vetor formatado
+void imprimirVetor(int senhas[], int tamanho) {
+    printf("[");
+    for (int i = 0; i < tamanho; i++) {
+        printf("%02d", senhas[i]);
+        if (i < tamanho - 1) {
+            printf(", ");
+        }
+    }
+    printf("]");
+}
 
-    for (i = 0; i < 5 - 1; i++) {
-        for (j = 0; j < 5 - 1 - i; j++) {
-            if (vet[j] > vet[j + 1]) {
-                temp = vet[j];
-                vet[j] = vet[j + 1];
-                vet[j + 1] = temp;
+// Função do Bubble Sort
+void bubbleSortRaioX(int senhas[], int tamanho) {
+    printf("Estado inicial da fila: ");
+    imprimirVetor(senhas, tamanho);
+    printf("\n");
+
+    // Laço externo: controla as passadas
+    for (int i = 0; i < tamanho - 1; i++) {
+
+        // Laço interno: faz as comparações
+        for (int j = 0; j < tamanho - i - 1; j++) {
+            if (senhas[j] > senhas[j + 1]) {
+                int aux = senhas[j];
+                senhas[j] = senhas[j + 1];
+                senhas[j + 1] = aux;
             }
         }
 
+        // Imprimindo estado após cada passada
         printf("Passo %d: ", i + 1);
-        for (int k = 0; k < 5; k++) {
-            printf("%d ", vet[k]);
-        }
+        imprimirVetor(senhas, tamanho);
         printf("\n");
     }
-
-    return 0;
 }
