63c342badf464e8b9448cb4af024cc48f36d2db1
Igor Vargas <igorlinke1997@gmail.com>
2026-05-03 22:47:57 -0300

    Bubble Sort em C com passos

--- bubble.c
+++ bubble.c
@@ -1,42 +1,23 @@
 #include <stdio.h>
 
-void bubbleSort(int arr[], int n) {
+int main() {
+    int vet[5] = {82, 45, 12, 78, 23};
     int i, j, temp;
-    int trocou;
 
-    for (i = 0; i < n - 1; i++) {
-        trocou = 0;
-
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // troca
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-
-                trocou = 1;
+    for (i = 0; i < 5 - 1; i++) {
+        for (j = 0; j < 5 - 1 - i; j++) {
+            if (vet[j] > vet[j + 1]) {
+                temp = vet[j];
+                vet[j] = vet[j + 1];
+                vet[j + 1] = temp;
             }
         }
-        if (trocou == 0)
-            break;
-    }
-}
 
-int main() {
-    int numeros[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = sizeof(numeros) / sizeof(numeros[0]);
-    int i;
-
-    printf("Antes: ");
-    for (i = 0; i < n; i++) {
-        printf("%d ", numeros[i]);
-    }
-
-    bubbleSort(numeros, n);
-
-    printf("\nDepois: ");
-    for (i = 0; i < n; i++) {
-        printf("%d ", numeros[i]);
+        printf("Passo %d: ", i + 1);
+        for (int k = 0; k < 5; k++) {
+            printf("%d ", vet[k]);
+        }
+        printf("\n");
     }
 
     return 0;
